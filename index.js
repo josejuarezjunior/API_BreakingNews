55a5bce62fb450bd6d226e83ac06e59d92e16143
Junior <Josejuarez.Junior@smartm.com>
2023-10-25 21:40:51 -0300

    HTTP Methods and routes

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
+/*
+ROTA
+    1) Método HTTP: 
+        GET - Pega uma info, 
+        POST - Cria uma info,
+        PUT - Altera toda a info,
+        PATCH - Altera parte da info,
+        DELETE - Apaga uma info
+
+    2) Nome - Um identificador da rota. Ex: '/home'
+
+    3) Function (Callback) - Responsável por executar algum comando.
+*/
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+    res.send('Hello World!');
 })
 
-app.listen(3000)
+app.listen(3000);
