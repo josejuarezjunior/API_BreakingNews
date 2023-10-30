16a57001c17fb6bd3cb119ac0a24a14b79729381
Junior <Josejuarez.Junior@smartm.com>
2023-10-30 08:16:35 -0300

    refactor: adding middlewares

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,4 @@
 const userService = require("../services/user.service");
-const mongoose = require("mongoose");
 
 const create = async (req, res) => {
   const { name, username, email, password, avatar, background } = req.body;
@@ -38,18 +37,10 @@ const findAll = async (req, res) => {
 };
 
 const findById = async (req, res) => {
-  const id = req.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({ message: "Invalid ID." });
-  }
-
-  const user = await userService.findByIdService(id);
-
-  if (!user) {
-    return res.status(400).send({ message: "User not found." });
-  }
-
+  //const id = req.params.id;
+  //const id = req.id; // Recebendo do Middleware 'validUser'.
+  //const user = await userService.findByIdService(id);
+  const user = req.user; // Recebendo do Middleware 'validUser'.
   res.send(user);
 };
 
@@ -60,17 +51,9 @@ const update = async (req, res) => {
     res.status(400).send({ message: 'Submit at least one field for update' });
   }
 
-  const id = req.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({ message: 'Invalid ID' });
-  }
-
-  const user = await userService.findByIdService(id);
-
-  if(!user) {
-    return res.status(400).send({ message: 'User not found' });
-  }
+  //const id = req.params.id;
+  //const user = await userService.findByIdService(id);
+  const { id, user } = req;// Recebendo do Middleware 'validUser'.
 
   await userService.updateService(
     id,
